e060a751747f662c0136a6fb92cd89f5ee8952cf
Nathalia Possato Ceolin <nathaliapossatoceolin@outlook.com>
2024-01-31 14:44:56 -0300

    cria menu hamburguer para versao mobile e secao numeros animais

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
-import initDropDownMenu from './modules/dropdownmenu.js';
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initDropDownMenu from "./modules/dropdownmenu.js";
+import initMenuMobile from "./modules/menumobile.js";
+import initAnimaNumeros from "./modules/animanumeros.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,3 +13,5 @@ initAccordion();
 initTabNav();
 initModal();
 initDropDownMenu();
+initMenuMobile();
+initAnimaNumeros();
